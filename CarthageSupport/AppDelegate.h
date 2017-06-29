4b99932ac5e1d7512025e05c925edaa004495fce
JokerYuan <1598004992@qq.com>
2017-06-29 16:21:07 +0800

    Initial Commit

---
+++ CarthageSupport/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  CarthageSupport
+//
+//  Created by joker on 2017/6/29.
+//  Copyright © 2017年 joker. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
